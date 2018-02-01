0e17b1c5440463576e71e139c49e50c98b71ad45
Daniel Winship <danielwinship@icloud.com>
2018-02-01 06:09:47 -0500

    fixes issue dismissing view controller back to starting GoalVC

--- GoalPost/Extensions/UIViewControllerExt.swift
+++ GoalPost/Extensions/UIViewControllerExt.swift
@@ -20,6 +20,19 @@ extension UIViewController {
         present(viewControllerToPresent, animated: false, completion: nil)
     }
     
+    func presentSecondayDetail(_ viewControllerToPresent:UIViewController) {
+        let transistion = CATransition()
+        transistion.duration = 0.3
+        transistion.type = kCATransitionPush
+        transistion.subtype = kCATransitionFromRight
+        
+        guard let presentedViewController = presentedViewController else {return}
+        presentedViewController.dismiss(animated: false) {
+            self.view.window?.layer.add(transistion, forKey: kCATransition)
+            self.present(viewControllerToPresent, animated: false, completion: nil)
+        }
+    }
+    
     func dismissDetail() {
         let transistion = CATransition()
         transistion.duration = 0.3
